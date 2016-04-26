0d983e5ff2522456f3efc0f7b791c6da8ecc8524
Your Name <you@example.com>
2016-04-26 09:38:29 +0600

    paver install

---
+++ paver-venv/bin/cog.py
@@ -0,0 +1,17 @@
+#!/home/bapon/jankins/jankins/paver-venv/bin/python
+""" Cog code generation tool.
+    http://nedbatchelder.com/code/cog
+
+    Copyright 2004-2015, Ned Batchelder.
+"""
+
+import time
+start = time.clock()
+
+import sys
+from cogapp import Cog
+
+ret = Cog().main(sys.argv)
+
+#print "Time: %.2f sec" % (time.clock() - start)
+sys.exit(ret)
